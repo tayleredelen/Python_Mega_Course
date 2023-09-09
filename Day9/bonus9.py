fee354ce111669048033635052db14e1861341f4
tayleredelen <taylerledelen@gmail.com>
2023-09-08 20:06:42 -0500

    Working on Day 9

---
+++ Day9/bonus9.py
@@ -0,0 +1,14 @@
+# password verifier
+
+password = input("Enter new password: ")
+
+result = []
+
+if len(password) >= 8:
+    result.append(True)
+else:
+    result.append(False)
+
+if password.isdigit()
+
+print(result)
