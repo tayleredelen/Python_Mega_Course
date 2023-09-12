367a35f095f49dda669d0a2f5968e83765b95f56
tayleredelen <taylerledelen@gmail.com>
2023-09-12 10:30:12 -0500

    Completed Day 13 reading

---
+++ ToDoApp/Day13/main13.py
@@ -0,0 +1,78 @@
+def get_todos(filepath="files/todos.txt"):
+    """ Read a text file and return a list
+    of todo list of items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath="files/todos.txt"):
+    """ Write todo list items in text file.
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+while True:
+    user_action = input("Type add, show, edit, complete, or exit: ")
+    user_action = user_action.strip()
+
+    if user_action.startswith('add'):
+        todo = user_action[4:]
+
+        todos = get_todos()
+
+        todos.append(todo + '\n')
+
+        write_todos(todos)
+
+    elif user_action.startswith("show"):
+        todos = get_todos()
+
+        new_todos = [item.strip('\n') for item in todos]
+        for index, item in enumerate(new_todos):
+            row = f"{index + 1}-{item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            print(number)
+            number = number - 1
+
+            todos = get_todos()
+
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo + '\n'
+
+            write_todos(todos)
+
+        except ValueError:
+            print("That command is not valid.")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = get_todos()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            write_todos(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
+    elif user_action.startswith("exit"):
+        break
+    else:
+        print("Command is not valid.")
+
+print("Bye! ツ")
