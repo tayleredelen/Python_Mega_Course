dcfc894bfe6798d22701694904c09a0db9bd41dc
tayleredelen <taylerledelen@gmail.com>
2023-09-13 11:09:43 -0500

    Completed Day 15 exercises

--- ToDoApp/Day15/bonus15.py
+++ ToDoApp/Day15/bonus15.py
@@ -1,1 +1,29 @@
-[{"question_text:""What are dolphins?", "alternatives": ["Amphibians", "Fish", "Mammals" "Birds"], "correct_answer":3}]
+import json
+
+with open("files/questions.json", "r") as file:
+    content = file.read()
+
+data = json.loads(content)
+
+for question in data:
+    print(question["question_text"])
+    for index, alternative in enumerate(question["alternatives"]):
+        print(index + 1, "-", alternative)
+    user_choice = int(input("Enter your answer: "))
+    question["user_choice"] = user_choice
+
+
+score = 0
+for index, question in enumerate(data):
+    if question["user_choice"]  == question["correct_answer"]:
+        score = score + 1
+        result = "Correct Answer"
+    else:
+        result = "Wrong Answer"
+
+    message = f"{index + 1} {result} - Your answer: {question['user_choice']}, " \
+              f"Correct answer: {question['correct_answer']}"
+    print(message)
+
+
+print(score, "/", len(data))
