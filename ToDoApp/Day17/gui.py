0d8b41f7ba4417b4b04f4f98b3feb2f61e5b9821
tayleredelen <taylerledelen@gmail.com>
2023-09-15 21:58:01 -0500

    Finished Day 17 Add Button

---
+++ ToDoApp/Day17/gui.py
@@ -0,0 +1,27 @@
+import functions
+import PySimpleGUI as sg
+# ^renaming sg for easier code
+
+label = sg.Text("Type in a ToDo: ")
+input_box = sg.InputText(tooltip="Enter ToDo", key="todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My ToDo App",
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
+window.close()
