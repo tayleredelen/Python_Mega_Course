d5015f11227df137a2778bb24a936bedef7111f4
tayleredelen <taylerledelen@gmail.com>
2023-09-15 21:42:31 -0500

    Finished Day 16 exercise

--- ToDoApp/Day16/exercise16.py
+++ ToDoApp/Day16/exercise16.py
@@ -2,15 +2,13 @@ import PySimpleGUI as sg
 
 label1 = sg.Text("Enter feet:")
 input1 = sg.Input()
-# ^special button programmed to select files
 
-label2 = sg.Text("Select destination folder:")
+label2 = sg.Text("Enter inches:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
 
-compress_button = sg.Button("Compress")
-window = sg.Window("File Compressor", layout=[[label1, input1, choose_button1],
-                                              [label2, input2, choose_button2],
-                                                [compress_button]])
+convert_button = sg.Button("Convert")
+window = sg.Window("Convertor", layout=[[label1, input1],
+                                              [label2, input2],
+                                                [convert_button]])
 window.read()
 window.close()
