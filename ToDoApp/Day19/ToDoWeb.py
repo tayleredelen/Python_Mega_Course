2332a11a43bcbe6aa308461d235de5f40c16c46c
tayleredelen <taylerledelen@gmail.com>
2023-09-25 11:37:08 -0500

    Finished Day 19 ToDoWeb complete feature

--- ToDoApp/Day19/ToDoWeb.py
+++ ToDoApp/Day19/ToDoWeb.py
@@ -9,15 +9,20 @@ def add_todo():
     todos.append(todo)
     functions.write_todos(todos)
 
+
 st.title("My ToDo App")
 st.subheader("This is my ToDo App")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add a new ToDo...",
               on_change=add_todo, key="new_todo")
-
 
 st.session_state
