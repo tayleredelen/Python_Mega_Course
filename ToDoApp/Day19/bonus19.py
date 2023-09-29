b5b339c7d19ba183cbb27fff61aad26cfa06787d
tayleredelen <taylerledelen@gmail.com>
2023-09-29 15:09:02 -0500

    Completed Day 19

--- ToDoApp/Day19/bonus19.py
+++ ToDoApp/Day19/bonus19.py
@@ -1,35 +1,16 @@
-import PySimpleGUI as sg
-from zip_extractor import extract_archive
+import streamlit as st
+from PIL import Image
 
-sg.theme("Black")
+with st.expander("Start camera"):
+    # start camera
+    camera_image = st.camera_input("Camera")
 
-label1 = sg.Text("Select archive:")
-input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose", key="archive")
-# ^special button programmed to select files
+if camera_image:
+    # create a pillow image instance
+    img = Image.open(camera_image)
 
-label2 = sg.Text("Select destination directory:")
-input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose", key="folder")
+    # convert the pillow image to grayscale
+    gray_img = img.convert("L")
 
-extract_button = sg.Button("Extract")
-output_label = sg.Text(key="output", text_color="green")
-
-window = sg.Window("Archive Extractor",
-                   layout=[[label1, input1, choose_button1],
-                           [label2, input2, choose_button2],
-                           [extract_button, output_label]])
-
-
-while True:
-    event, values = window.read()
-    print(event, values)
-    archivepath = values["archive"]
-    dest_dir = values["folder"]
-    extract_archive(archivepath, dest_dir)
-    window["output"].update(value="Extraction Completed!")
-
-window.read()
-window.close()
-
-
+    # render the grayscale image on webpage
+    st.image(gray_img)
